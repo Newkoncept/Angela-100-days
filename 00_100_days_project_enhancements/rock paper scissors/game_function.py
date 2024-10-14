b9ee72e34326b74f0a8e82f9e5b4465622be399f
Oluwagbemiga <taiwoemmanuel15@gmail.com>
2024-10-14 20:56:34 +0100

    Added multiplayer and diagram logic
    This commit adds the following functionality:
    - A multiplayer and single player functionality where uses can play with the computer or be able with their friends
    - The multiplayer functionality allows for 2 different players to play with each other
    - Made the diagram show beside each other instead of stacking it upon each other

--- 00_100_days_project_enhancements/rock paper scissors/game_function.py
+++ 00_100_days_project_enhancements/rock paper scissors/game_function.py
@@ -31,61 +31,84 @@ def print_appropriate_diagram(diagram, value):
 
 
 # This function handles the logic in determining the result of a round
-def game_result(user, computer):
+def game_result(user, computer, game_plan):
     # rock = 0 # paper = 1 # scissors = 2
 
+    #GAME RULES
     # Rock wins against scissors.
     # Scissors win against paper.
     # Paper wins against rock.
 
     result = ""
-
-    # colored_text("Draw!!", color_code)
-    # colored_text("You win!!", "31")
-    # colored_text("You lose!!", "32")
     if user == computer:
         result = colored_text("DRAW!!", "33")
+    else:
+        if game_plan == 1:
+            if user == 0 and computer == 2:
+                result = colored_text("Player 1 win!!", "32")
+            elif user == 2 and computer == 0:
+                result = colored_text("Player 2 win!!", "32")
 
-    elif user == 0 and computer == 2:
-        result = colored_text("You win!!", "32")
-    elif user == 2 and computer == 0:
-        result = colored_text("You lose!!", "31")
+            elif user == 2 and computer == 1:
+                result = colored_text("Player 1 win!!", "32")
+            elif user == 1 and computer == 2:
+                result = colored_text("Player 2 win!!", "32")
 
-    elif user == 2 and computer == 1:
-        result = colored_text("You win!!", "32")
-    elif user == 1 and computer == 2:
-        result = colored_text("You lose!!", "31")
+            elif user == 1 and computer == 0:
+                result = colored_text("Player 1 win!!", "32")
+            elif user == 0 and computer == 1:
+                result = colored_text("Player 2 win!!", "32")
 
-    elif user == 1 and computer == 0:
-        result = colored_text("You win!!", "32")
-    elif user == 0 and computer == 1:
-        result = colored_text("You lose!!", "31")
+        else:
+            if user == computer:
+                result = colored_text("DRAW!!", "33")
+
+            elif user == 0 and computer == 2:
+                result = colored_text("You win!!", "32")
+            elif user == 2 and computer == 0:
+                result = colored_text("You lose!!", "31")
+
+            elif user == 2 and computer == 1:
+                result = colored_text("You win!!", "32")
+            elif user == 1 and computer == 2:
+                result = colored_text("You lose!!", "31")
+
+            elif user == 1 and computer == 0:
+                result = colored_text("You win!!", "32")
+            elif user == 0 and computer == 1:
+                result = colored_text("You lose!!", "31")
 
     return result
 
+
 # This function display the diagram in relation to the program input
 # and shows them beside each other (UNDER-DEVELOPMENT)
-def modified_print_appropriate_diagram(diagram, value, player):
+def modified_print_appropriate_diagram(diagram, user_value, computer_value):
     # rock = 0
     # paper = 1
     # scissors = 2
 
     result = ""
+    user_diagram = None
+    computer_diagram = None
 
-    if player == "user":
-        if value == 0:
-            result = diagram.user_rock
-        elif value == 1:
-            result = diagram.user_paper
-        elif value == 2:
-            result = diagram.user_scissor
+    if user_value == 0:
+        user_diagram = diagram.user_rock
+    elif user_value == 1:
+        user_diagram = diagram.user_paper
+    elif user_value == 2:
+        user_diagram = diagram.user_scissor
 
-    elif player == "computer":
-        if value == 0:
-            result = diagram.computer_rock
-        elif value == 1:
-            result = diagram.computer_paper
-        elif value == 2:
-            result = diagram.computer_scissor
+    if computer_value == 0:
+        computer_diagram = diagram.computer_rock
+    elif computer_value == 1:
+        computer_diagram = diagram.computer_paper
+    elif computer_value == 2:
+        computer_diagram = diagram.computer_scissor
+
+    for i in range(6):
+        print(user_diagram[i], end="\t\t\t\t")
+        print(computer_diagram[i])
+
 
     return result
--- 00_100_days_project_enhancements/rock paper scissors/main.py
+++ 00_100_days_project_enhancements/rock paper scissors/main.py
@@ -10,24 +10,51 @@ Adding congratulatory gif or ascii art
 Making the diagram show beside each other
 
 '''
-computer_choice = randint(0, 2)
-user_choice = int(input("""What do you choose?
-    Type
-    0 for Rock
-    1 for Paper
-    2 for Scissors\n"""))
+game_plan = int(input("""Welcome to the game!!!
+    Do you want:
+    1. Multiplayer (2 player)
+    2. Single player\n"""))
 
-
-if user_choice > 2 or user_choice < 0:
+if game_plan > 2 or game_plan < 1:
     print(f"\nError: Invalid entry!!!!")
 else:
-    print("You chose:")
-    print(game_function.print_appropriate_diagram(diagram, user_choice))
+    if game_plan == 1:
+        user1_choice = int(input("""User 1 Player: What do you choose?
+        Type
+        0 for Rock
+        1 for Paper
+        2 for Scissors\n"""))
 
-    print("\nComputer chose:")
-    print(game_function.print_appropriate_diagram(diagram, computer_choice))
+        user2_choice = int(input("""User 2 Player: What do you choose?
+        Type
+        0 for Rock
+        1 for Paper
+        2 for Scissors\n"""))
 
-    print(f"\n\nResult: {game_function.game_result(user_choice, computer_choice)}")
+        if user1_choice > 2 or user1_choice < 0 :
+            print(f"\nError: User 1 entered an invalid entry!!!!")
+        elif user2_choice > 2 or user2_choice < 0 :
+            print(f"\nError: User 2 entered an invalid entry!!!!")
+
+        else:
+            print("User1 chose: \t\t\t\t\t User2 chose:")
+            print(game_function.modified_print_appropriate_diagram(diagram, user1_choice, user2_choice))
+
+            print(f"\nResult: {game_function.game_result(user1_choice, user2_choice, game_plan)}")
+
+    else:
+        computer_choice = randint(0, 2)
+        user_choice = int(input("""What do you choose?
+            Type
+            0 for Rock
+            1 for Paper
+            2 for Scissors\n"""))
 
 
+        if user_choice > 2 or user_choice < 0:
+            print(f"\nError: Invalid entry!!!!")
+        else:
+            print("User1 chose: \t\t\t\t\t Computer chose:")
+            print(game_function.modified_print_appropriate_diagram(diagram, user_choice, computer_choice))
 
+            print(f"\nResult: {game_function.game_result(user_choice, computer_choice, game_plan)}")
