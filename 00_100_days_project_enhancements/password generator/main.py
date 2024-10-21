d288b968d6959b4253a9ea9d0ca48108c619a0ab
Oluwagbemiga <taiwoemmanuel15@gmail.com>
2024-10-21 17:01:21 +0100

    Created a modified password generator
    This commit allows users to generate randomized password
    - It shuffles the password 3 times to ensure that the final password is randomized

---
+++ 00_100_days_project_enhancements/password generator/main.py
@@ -0,0 +1,38 @@
+import random, password_content
+
+"""
+Randomize 3 three times
+Show the final password
+Show the total length of generated values
+
+Advance:
+Specify the content
+Use the auto generator
+Use the auto generator with user specifying the required values
+"""
+
+print("Welcome to the PyPassword Generator")
+user_letters = int(input("How many letters would you like in your password?\n"))
+user_numbers = int(input("How many numbers would you like?\n"))
+user_symbols = int(input("How many symbols would you like?\n"))
+
+
+generated_value = []
+
+generated_value.extend(random.choices(password_content.letters, k=user_letters))
+generated_value.extend(random.choices(password_content.numbers, k=user_numbers))
+generated_value.extend(random.choices(password_content.symbols, k=user_symbols))
+
+print(generated_value)
+for i in range(0, 3):
+    random.shuffle(generated_value)
+
+print(generated_value)
+
+
+final_password = ''.join(generated_value)
+print(f"Your password is: {final_password}")
+
+
+
+#  total length
