969917adc139461ff1f2e538ba0cb0e7be73b832
Oluwagbemiga <taiwoemmanuel15@gmail.com>
2024-10-25 07:13:57 +0100

    Add documentation to functions

--- higher lower game/function.py
+++ higher lower game/function.py
@@ -1,12 +1,15 @@
 from data import data_set
 import random, art
 
+
 def content_picker():
+    """This function picks a question from the lists of available questions"""
     game_category = random.choice(list(data_set.keys()))
     return random.choice(data_set[game_category])
 
 
 def generate_question():
+    """This function format the picked question in a usable format for further processing"""
     question = content_picker()
     name = question["name"]
     description = question["description"].replace(".", "")
@@ -18,6 +21,9 @@ def generate_question():
 
 
 def generate_all_questions():
+    """This functions return all the necessary questions for processing,
+    and it also performs error checking to prevent the display of the same question
+    at the same time"""
     first_question = generate_question()
     second_question = generate_question()
 
@@ -29,6 +35,8 @@ def generate_all_questions():
 
 
 def display_question(first_run, first_question, second_question, score):
+    """This function performs the functionality of displaying the questions,
+    necessary art and the current score of the user when the user gets the last question"""
     print(art.logo)
 
     if not first_run:
@@ -40,6 +48,8 @@ def display_question(first_run, first_question, second_question, score):
 
 
 def user_pick_answer(first_question, second_question):
+    """This function handles the logic of determining if the value picked
+    by the user is the correct or wrong answer"""
     user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
 
     if user_choice == "A":
